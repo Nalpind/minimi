d66950c3d5cc45f00ed32d773d593790e0395c7f
Nalpind <gpendras@gmail.com>
2026-09-12 20:18:15 -0700

    Preload glb file

--- src/components/Asset.jsx
+++ src/components/Asset.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import { useZStore } from '../store/useZStore'
+
 
 export function Asset({ assetId }) {
-    const { assetList } = useZStore()
     return (
         <>
             <button className="pointer-events-auto flex aspect-square w-full rounded-lg bg-slate-600 py-0.5 pr-0.5" onClick={() => console.log(assetId)}>
