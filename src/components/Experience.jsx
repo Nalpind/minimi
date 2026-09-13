ed29d5db03d9faa148bcd3e57c011eb995584e4d
Nalpind <gpendras@gmail.com>
2026-09-12 20:45:52 -0700

    preload asset added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,28 +1,42 @@
-import { OrbitControls, useGLTF } from '@react-three/drei';
-import { useZStore } from '../store/useZStore';
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useZStore } from "../store/useZStore";
+import { Suspense } from "react";
+import { Preload } from "@react-three/drei";
 
 function Model(props) {
-  const [body, head] = useGLTF.preload(['/3D/Body.glb', '/3D/Head.glb'])
-
+  const [body, head] = useGLTF(["/3D/Body.glb", "/3D/Head.glb"]);
   return (
     <group>
       <primitive object={body.scene} {...props} />
       <primitive object={head.scene} {...props} />
     </group>
-  )
+  );
 }
 
 export const Experience = () => {
   const { active } = useZStore();
+  useGLTF.preload(["/3D/Body.glb", "/3D/Head.glb"]);
 
   return (
     <>
-      <color attach="background" args={['#0f172a']} />
+      <color attach="background" args={["#0f172a"]} />
       <ambientLight intensity={1.4} />
       <directionalLight position={[3, 4, 5]} intensity={2} />
-      <OrbitControls enablePan={false} maxPolarAngle={Math.PI * 0.9} minPolarAngle={Math.PI * 0.2} />
-      {active === 'hat' ? <mesh><boxGeometry args={[1, 1, 1]} /></mesh> : <Model position={[0, -1, 0]} />}
+      <OrbitControls
+        enablePan={false}
+        maxPolarAngle={Math.PI * 0.9}
+        minPolarAngle={Math.PI * 0.2}
+      />
+      {active === "hat" ? (
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+        </mesh>
+      ) : (
+        <Suspense>
+          <Preload all />
+          <Model position={[0, -1, 0]} />
+        </Suspense>
+      )}
     </>
   );
 };
-
